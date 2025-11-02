903ec7aa9d4c9b1d7bd91c0f4e5693bcb9f6d6fc
HarrisonVanderZee <harrison.vanderzee2@gmail.com>
2025-11-02 22:38:29 +0000

    finished Assignment3

--- src/components/Syrup.vue
+++ src/components/Syrup.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="syrup" v-if="currentSyrup.id === 's1'" :style="{backgroundColor: syrups[0].color}"></div>
-  <div class="syrup" v-if="currentSyrup.id === 's2'" :style="{backgroundColor: syrups[1].color}"></div>
-  <div class="syrup" v-if="currentSyrup.id === 's3'" :style="{backgroundColor: syrups[2].color}"></div>
-  <div class="syrup" v-if="currentSyrup.id === 's4'" :style="{backgroundColor: syrups[3].color}"></div>
+  <div class="syrup" v-if="currentSyrup?.id === 's1'" :style="{backgroundColor: syrups[0].color}"></div>
+  <div class="syrup" v-if="currentSyrup?.id === 's2'" :style="{backgroundColor: syrups[1].color}"></div>
+  <div class="syrup" v-if="currentSyrup?.id === 's3'" :style="{backgroundColor: syrups[2].color}"></div>
+  <div class="syrup" v-if="currentSyrup?.id === 's4'" :style="{backgroundColor: syrups[3].color}"></div>
 </template>
 
 <script setup lang="ts">
-  import { currentSyrup, syrups} from "../stores/beverage";
+import { computed } from 'vue'
+import { syrupStore } from "../stores/beverage";
+const syrupState = syrupStore();
+const currentSyrup = computed(() => syrupState.currentSyrup);
+const syrups = syrupState.syrups;
 </script>
 <style lang="scss" scoped>
 .syrup {
